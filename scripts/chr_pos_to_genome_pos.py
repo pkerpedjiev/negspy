c72507f069e6c39e82f31a54a46f5b2ca0d48c77
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-05-18 09:37:37 -0400

    Created a script that works in streaming mode

--- scripts/chr_pos_to_genome_pos.py
+++ scripts/chr_pos_to_genome_pos.py
@@ -4,17 +4,23 @@ import negspy.coordinates as nc
 import sys
 import argparse
 
+from itertools import tee, izip
+
+def pairwise(iterable):
+    "s -> (s0, s1), (s2, s3), (s4, s5), ..."
+    a = iter(iterable)
+    return izip(a, a)
+
 def main():
     parser = argparse.ArgumentParser(description="""
     
-    python chr_pos_to_genome_pos.py chr pos
+    python chr_pos_to_genome_pos.py 
 
-    Convert a chromosome,position pair to a genome position. Assumes that the coordinates
-    refer to the hg19 assembly.
+    Convert chromosome,position pairs to genome_positions. Assumes that the
+    coordinates refer to the hg19 assembly.
+
 """)
 
-    parser.add_argument('chr', nargs=1)
-    parser.add_argument('pos', nargs=1, type=int)
     parser.add_argument('-a', '--assembly', default='hg19')
 
 
@@ -22,10 +28,17 @@ def main():
     #                     help="Some option", type='str')
     #parser.add_argument('-u', '--useless', action='store_true', 
     #                     help='Another useless option')
-
     args = parser.parse_args()
 
-    print nc.chr_pos_to_genome_pos(args.chr[0], args.pos[0], args.assembly)
+    for line in sys.stdin:
+        try:
+            line_output = []
+            for chrom, pos in pairwise(line.strip().split()):
+                genome_pos = nc.chr_pos_to_genome_pos( chrom, int(pos), args.assembly)
+                line_output += [genome_pos]
+            print " ".join(map(str, line_output))
+        except KeyError as ke:
+            print >>sys.stderr, "KeyError:", ke, line.strip()
     
 
 if __name__ == '__main__':
