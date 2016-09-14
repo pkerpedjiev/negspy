bb1cda57917ecec4d87802048daed574976faab0
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-09-14 20:08:15 +0000

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='negspy',
-      version='0.1.1',
+      version='0.1.2',
       description='Python NGS tools',
       author='Peter Kerpedjiev',
       author_email='pkerpedjiev@gmail.com',
