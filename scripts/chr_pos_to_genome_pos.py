4aad9b145e109c3b669bec829b134710af5d0347
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-10-02 17:02:29 +0000

    Specify which columns to translate

--- scripts/chr_pos_to_genome_pos.py
+++ scripts/chr_pos_to_genome_pos.py
@@ -15,7 +15,7 @@ def pairwise(iterable):
 def main():
     parser = argparse.ArgumentParser(description="""
     
-    python chr_pos_to_genome_pos.py 
+    python chr_pos_to_genome_pos.py -t 1,2:3,4
 
     Convert chromosome,position pairs to genome_positions. Assumes that the
     coordinates refer to the hg19 assembly.
@@ -23,10 +23,9 @@ def main():
 """)
 
     parser.add_argument('-a', '--assembly', default='hg19')
+    parser.add_argument('-c', '--columns', default='1,2', 
+            help="Which columns to translate to genome positions. Column pairs should be separated by colons")
 
-
-    parser.add_argument('-e', '--extra-columns', default='',
-                         help="Some option", type=str)
     #parser.add_argument('-u', '--useless', action='store_true', 
     #                     help='Another useless option')
     args = parser.parse_args()
@@ -35,15 +34,21 @@ def main():
         try:
             line_output = []
             line_parts = line.strip().split()
-            for chrom, pos in pairwise(line_parts):
+            translated = set()
+            for translate_pair in [[int (y) for y in x.split(',')] for x in args.columns.split(':')]:
+                # go through the pairs of columns that need to be translated to genome position
+                chrom,pos = line_parts[translate_pair[0]-1], line_parts[translate_pair[1]-1]
                 genome_pos = nc.chr_pos_to_genome_pos( chrom, int(pos), args.assembly)
                 line_output += [genome_pos]
-            for col in args.extra_columns.split(','):
-                try:
-                    line_output += [line_parts[int(col)-1]]
-                except IndexError:
-                    print("Invalid input, not enough columns:", line, file=sys.stderr)
-                    break
+
+                # note that we've translated these columns and shouldn't include them in the output
+                translated.add(translate_pair[0]-1)
+                translated.add(translate_pair[1]-1)
+
+            for i,part in enumerate(line_parts):
+                if i not in translated:
+                    line_output += [part]
+
             print("\t".join(map(str, line_output)))
         except KeyError as ke:
             print("KeyError:", ke, line.strip(), file=sys.stderr)
