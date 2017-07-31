e32ccb8106257efd13cbe9f1eb64c0ba81737df2
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-07-31 11:35:13 -0400

    Bump version: 0.2.15 → 0.2.16

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='negspy',
-      version='0.2.15',
+      version='0.2.16',
       description='Python NGS tools',
       author='Peter Kerpedjiev',
       author_email='pkerpedjiev@gmail.com',
